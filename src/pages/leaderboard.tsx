4fe3c948774909bb82ff629d18e0b4a665b08cf6
TheMath123 <mat.matheus_pa@hotmail.com>
2021-04-16 20:18:02 -0300

    Interface leaderboard finalizada, inicio da conexão com banco de dados, e inicio da autentificação.

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -9,13 +9,65 @@ export default function Leaderboard() {
             <header>
                 <span>Leaderboard</span>
                 <div className={styles.headerLeaderboard}>
-                    <p>Posição</p>
-                    <p>Usuário</p>
-                    <p>Desafios</p>
-                    <p>Experiência</p>
+                    <div className={styles.headerLeftSide}>
+                        <p>Posição</p>
+                        <p>Usuário</p>
+                    </div>
+                    <div className={styles.headerRightSide}>
+                        <p>Desafios</p>
+                        <p>Experiência</p>
+                    </div>
                 </div>
             </header>
             <div className={styles.rankingBox}>
+                <RankingPositionBox 
+                    rankingPosition={1}
+                    name="Matheus P Agostinho"
+                    avatar="https://avatars.githubusercontent.com/u/28405781?v=4"
+                    level={25}
+                    challengeCompleted={35}
+                    totalExperience={12356789}
+                />
+                <RankingPositionBox 
+                    rankingPosition={2}
+                    name="Alpha Vylly"
+                    avatar="https://avatars.githubusercontent.com/u/65187737?v=4"
+                    level={15}
+                    challengeCompleted={25}
+                    totalExperience={123567}
+                />
+                <RankingPositionBox 
+                    rankingPosition={1}
+                    name="Matheus P Agostinho"
+                    avatar="https://avatars.githubusercontent.com/u/28405781?v=4"
+                    level={25}
+                    challengeCompleted={35}
+                    totalExperience={12356789}
+                />
+                <RankingPositionBox 
+                    rankingPosition={2}
+                    name="Alpha Vylly"
+                    avatar="https://avatars.githubusercontent.com/u/65187737?v=4"
+                    level={15}
+                    challengeCompleted={25}
+                    totalExperience={123567}
+                />
+                <RankingPositionBox 
+                    rankingPosition={1}
+                    name="Matheus P Agostinho"
+                    avatar="https://avatars.githubusercontent.com/u/28405781?v=4"
+                    level={25}
+                    challengeCompleted={35}
+                    totalExperience={12356789}
+                />
+                <RankingPositionBox 
+                    rankingPosition={2}
+                    name="Alpha Vylly"
+                    avatar="https://avatars.githubusercontent.com/u/65187737?v=4"
+                    level={15}
+                    challengeCompleted={25}
+                    totalExperience={123567}
+                />
                 <RankingPositionBox 
                     rankingPosition={1}
                     name="Matheus P Agostinho"
